f1f26e88e251d42bc1ed6bab0a1b4ad5b900c24e
Ellaylone <heamikallove@gmail.com>
2016-05-30 12:43:18 +0000

    fuck this redactor

---
+++ web/js/admin/site.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(function () {
+        $("[data-toggle='tooltip']").tooltip();
+    });
+    $(function () {
+        $("[data-toggle='popover']").popover();
+    });
+});
